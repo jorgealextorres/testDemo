ada157584838df624dd29b6362d782792451d40a
jorge <jorgealextorres@yahoo.es>
2019-11-23 09:39:15 +0100

    test hecho2

---
+++ src/test/java/com/lunes/test/TestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunes.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
